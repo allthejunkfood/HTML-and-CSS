06b75e315ab6a46042dd24133a670c1094638706
ChiefBitCounter <rconley6836@comcast.net>
2020-04-09 18:07:21 -0400

    reorganize Repo

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+window.alert("RED ALARM!");
+
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = result;
+}
+
+document.getElementById("color1").style.textDecorationColor = "Black";
+
+document.getElementById("color2").style.textDecorationColor = "Blue";
